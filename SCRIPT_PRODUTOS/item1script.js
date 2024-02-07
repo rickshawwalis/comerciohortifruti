aaacd26fd78ba18ea90ec76b175e2437e2c95a24
rickshawwalis <rickshawwallis@gmail.com>
2024-02-07 19:02:54 -0300

    ass

--- SCRIPT_PRODUTOS/item1script.js
+++ SCRIPT_PRODUTOS/item1script.js
@@ -1,35 +1,43 @@
-
 //SCRIPT PARA COLOCAR QUANTIDADE DE ITEM A SER PEDIDAS
-    const ValorDisplay = document.getElementById('displayInput');
+const ValorDisplay = document.getElementById('displayInput');
+let value = parseInt(ValorDisplay.value); // Move a declaração para fora dos eventos de clique
 
-    const botaoMais = document.getElementById('botaoMAIS');
-    botaoMais.addEventListener('click', () => {
-        let value = parseInt(ValorDisplay.value);
-        if (!isNaN(value)) {
-            value++;
-            ValorDisplay.value = value;
-            // Armazena o valor atual no sessionStorage
-            sessionStorage.setItem('itemValue', value);
-        } else {
-            // Se o valor não for numérico, define como 0 e incrementa
-            ValorDisplay.value = 0;
-            sessionStorage.setItem('itemValue', 0);
-        }
-    });
+const botaoMais = document.getElementById('botaoMAIS');
+botaoMais.addEventListener('click', () => {
+    if (isNaN(value)) {
+        value = 1;
+    } else {
+        value++;
+    }
+    ValorDisplay.value = value;
+    // Armazena o valor atual no sessionStorage
+    sessionStorage.setItem('itemValue', value);
+});
+
+const botaoMenos = document.getElementById('botaoMENOS');
+botaoMenos.addEventListener('click', () => {
+    if (value > 0) {
+        value--;
+    }
+    ValorDisplay.value = value;
+    // Armazena o valor atual no sessionStorage
+    sessionStorage.setItem('itemValue', value);
+});
+
+const validacoes = () => {
+    if (ValorDisplay.value === "" || ValorDisplay.value == 0) {
+        alert("Informe a quantidade!");
+    } else {
+        alert("Produto Armazenado no carrinho");
+        location.reload();
+    }
     
-    const botaoMenos = document.getElementById('botaoMENOS');
-    botaoMenos.addEventListener('click', () => {
-        let value = parseInt(ValorDisplay.value);
-        if (!isNaN(value) && value > 0) {
-            value--;
-            ValorDisplay.value = value;
-            // Armazena o valor atual no sessionStorage
-            sessionStorage.setItem('itemValue', value);
-        } else {
-            // Se o valor não for numérico ou menor que zero, define como 0
-            ValorDisplay.value = 0;
-            sessionStorage.setItem('itemValue', 0);
-        }
-    });
-    
-   
+}    
+
+document.querySelector("#ComprarProduto").addEventListener("click", () => {
+    validacoes()
+})
+
+
+
+
