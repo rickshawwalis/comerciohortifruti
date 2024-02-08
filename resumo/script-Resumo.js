893d954ea6318f8f31d444a920c7abc828461d00
rickshawwalis <rickshawwallis@gmail.com>
2024-02-08 13:29:16 -0300

    Squashed commit of the following:
    commit 3393fc2baad55ca102190b1596c497a33ec93b8c
    Author: rickshawwalis <rickshawwallis@gmail.com>
    Date:   Thu Feb 8 13:29:03 2024 -0300
    
        ds
    
    commit 87c18d37778cfe52ee2029dc9eebe74f96575a9a
    Author: rickshawwalis <rickshawwallis@gmail.com>
    Date:   Thu Feb 8 12:49:23 2024 -0300
    
        hh
    
    commit 05ea6897e4e9bedee3ad135e4794eeeb9fc80abc
    Author: rickshawwalis <rickshawwallis@gmail.com>
    Date:   Thu Feb 8 11:08:23 2024 -0300
    
        asd
    
    commit d4c71f5cd9cdbb6e9ca02d5aa80fdc74dc67b212
    Author: rickshawwalis <rickshawwallis@gmail.com>
    Date:   Thu Feb 8 09:47:54 2024 -0300
    
        asd
    
    commit 42e5e3a42f73ea920e154a6a73bba4336dff2101
    Author: rickshawwalis <rickshawwallis@gmail.com>
    Date:   Thu Feb 8 08:22:07 2024 -0300
    
        d
    
    commit 05cc4c00974aea2d65942940becf1dbd9889ad66
    Author: rickshawwalis <rickshawwallis@gmail.com>
    Date:   Wed Feb 7 19:14:10 2024 -0300
    
        as

---
+++ resumo/script-Resumo.js
@@ -0,0 +1,77 @@
+const carrinhoCompras = () => {
+  let somaGeral = 0
+  const container = document.querySelector('.conteudo');
+
+  const Apagar = (div, chaveProduto, chaveValor,chaveQuantidade) => {
+    let excluir = document.createElement('button');
+    excluir.setAttribute("class", "excluirItem");
+    excluir.innerHTML = "REMOVER &#10060";
+    div.appendChild(excluir);
+
+
+    excluir.addEventListener('click', () => {
+
+      //multiplica para depois subtrair o valor que foi excluido
+      const valorDivRemovida = parseFloat(sessionStorage.getItem(chaveValor)) * parseFloat(sessionStorage.getItem(chaveQuantidade));
+      
+      container.removeChild(div); // Remove a div que contém o botão
+      
+      removerSessionStorage(chaveProduto, chaveValor);
+      // Subtrai o valor da div removida do somaGeral
+      somaGeral -= valorDivRemovida;
+
+      document.querySelector(".valor").innerHTML = `<span style="font-weight: bold;">VALOR GERAL:</span> R$ ${somaGeral.toFixed(2)}`;
+
+    });
+  };
+
+  const removerSessionStorage = (chaveProduto, chaveValor) => {
+    sessionStorage.removeItem(chaveProduto);
+    sessionStorage.removeItem(chaveValor);
+  };
+
+  const calcular = (escolhaValor, escolhaQuantidade) => {
+    somaGeral += escolhaValor * escolhaQuantidade;
+  };
+
+  const CriaDiv = () => {
+    for (let i = 0; i < sessionStorage.length; i++) {
+      const chaveProduto = `nome_Produto_${i}`;
+      const chaveValor = `valor_Produto_${i}`;
+      const chaveQuantidade = `quantidadeProduto_${i}`;
+
+      const escolhaProduto = sessionStorage.getItem(chaveProduto);
+      const escolhaValor = parseFloat(sessionStorage.getItem(chaveValor));
+      const escolhaQuantidade = parseInt(sessionStorage.getItem(chaveQuantidade));
+
+  
+      if (escolhaProduto) {
+        let div = document.createElement('div');
+        div.setAttribute("class", "mercadoria");
+       
+        calcular(escolhaValor, escolhaQuantidade);
+
+        let somaTotal = escolhaValor * escolhaQuantidade;
+
+        // Exiba os valores formatados no HTML
+        div.innerHTML += `
+ <br> <br><span style="font-weight: bold;"> &#127814;&#129365;&#129477;&#129365; PRODUTO:</span> <br>&#10145; ${escolhaProduto}
+ <br> <br><span style="font-weight: bold;">&#128178;VALOR PRODUTO:</span> <br>&#10145; R$ ${escolhaValor.toFixed(2)}
+ <br> <br><span style="font-weight: bold;">&#128204;QUANTIDADE:</span> <br>&#10145; ${escolhaQuantidade}
+ <br> <br><span style="font-weight: bold;">&#128181;VALOR TOTAL:</span> <br>&#10145; R$ ${somaTotal.toFixed(2)}
+   `;
+
+        container.appendChild(div);
+        Apagar(div, chaveProduto, chaveValor,chaveQuantidade);
+
+      
+      }
+    }
+    
+  }
+
+
+  CriaDiv()
+  document.querySelector(".valor").innerHTML = `<span style="font-weight: bold;">&#128181 VALOR GERAL:</span> R$ ${somaGeral.toFixed(2)} &#128181`;
+}
+document.addEventListener('DOMContentLoaded', carrinhoCompras);
